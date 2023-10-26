a50995d33536cd600238bdcf16fd3fb421750132
rafiechautie <rafiqauti13@gmail.com>
2023-10-26 16:03:09 +0700

    feat(FSB3-2): 1. menambahkan halaman login page versi mobile dan versi dekktop 2. folder image berada di dalam assets 3. terdapat js untuk membuat toogle eye

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const passwordField = document.getElementById("password");
+const togglePassword = document.getElementById("togglePassword");
+
+console.log(togglePassword)
+
+togglePassword.addEventListener("click", function () {
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    togglePassword.classList.remove("fa-eye");
+    togglePassword.classList.add("fa-eye-slash");
+  } else {
+    passwordField.type = "password";
+    togglePassword.classList.remove("fa-eye-slash");
+    togglePassword.classList.add("fa-eye");
+  }
+});
